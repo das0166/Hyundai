cee9b28be4c7f64708246c14503051d94fb2a70f
das0166 <das0166@naver.com>
2023-03-20 21:41:36 +0900

    animation

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$( document ).ready( function() {
+    $( '.swiper-slide-next' ).has( '.card_none' ).css( 'pointer-events', 'auto' );
+    //  if($( '.swiper-wrapper' ).has( '.swiper-slide-next' )){
+         
+    //  }else{
+    //     $( '.swiper-slide' ).has( '.card_none' ).css( 'pointer-events', 'none' );
+    //  }
+  } );
+
+  $

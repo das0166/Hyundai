72e7a8e6331d65f5ae70bcae02569ae4eeb87bf7
das0166 <das0166@naver.com>
2023-03-22 18:26:58 +0900

    반응형

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 $( document ).ready( function() {
-    $( '.swiper-slide-next' ).has( '.card_none' ).css( 'pointer-events', 'auto' );
+    // $( '.swiper-slide-next' ).has( '.card_none' ).css( 'pointer-events', 'auto' );
     //  if($( '.swiper-wrapper' ).has( '.swiper-slide-next' )){
          
     //  }else{
     //     $( '.swiper-slide' ).has( '.card_none' ).css( 'pointer-events', 'none' );
     //  }
+    // 
   } );
 
-  $
+  // if(document.readyState=="loading"){
+  //   location.href='/';
+  //   }
+// location.reload();
+// location.href='';
+// 
+// location.reload(true);
+window.onload = function() {
+  setTimeout (function () {
+  scrollTo(0,0);
+  }, 100);
+  }
